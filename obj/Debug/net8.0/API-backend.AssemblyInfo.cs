fbb30b556ec7f789a1c35b7c37149ca58932cd49
justinh225 <jah00031@mix.wvu.edu>
2024-10-12 20:18:50 -0400

    Updates

--- obj/Debug/net8.0/API-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/API-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a2a9e4859a2506cfc82a9e446c482b766973c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa8c7ced2c83a780b9974bc4c267414f377552c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
